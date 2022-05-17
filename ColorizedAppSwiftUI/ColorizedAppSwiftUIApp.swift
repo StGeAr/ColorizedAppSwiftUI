b2a0a3bcfa4cf4308b100f5ddb3ee6f93628b5a9
German Stavitskiy <gera_stavickii@mail,.ru>
2022-05-17 11:34:58 +0300

    Initial Commit

---
+++ ColorizedAppSwiftUI/ColorizedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUIApp.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by Герман Ставицкий on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
